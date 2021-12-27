e372d19238d847fe58f45108c26fe47c173a677e
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-12-26 21:27:59 -0700

    Finished "error" assignment

--- mysqlconnection.py
+++ mysqlconnection.py
@@ -5,8 +5,8 @@ class MySQLConnection:
     def __init__(self, db):
         # change the user and password as needed
         connection = pymysql.connect(host = 'localhost',
-                                    user = 'root', 
-                                    password = 'root', 
+                                    user = 'root',
+                                    password = 'rootroot',
                                     db = db,
                                     charset = 'utf8mb4',
                                     cursorclass = pymysql.cursors.DictCursor,
@@ -19,7 +19,7 @@ class MySQLConnection:
             try:
                 query = cursor.mogrify(query, data)
                 print("Running Query:", query)
-     
+
                 cursor.execute(query, data)
                 if query.lower().find("insert") >= 0:
                     # INSERT queries will return the ID NUMBER of the row inserted
